6ac523b27a8331e88583748f6b6352dc22345a2f
Peter Brunner <peter@lugoues.net>
2025-10-10 12:04:34 -0400

    auto update

--- chezmoiroot/.chezmoiscripts/run_after_00-mise.sh
+++ chezmoiroot/.chezmoiscripts/run_after_00-mise.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.local/{etc,bin,share/zsh/{init.d,completion}}
 
-~/.local/bin/mise self-update
+~/.local/bin/mise self-update --yes
 
 # Mise doesn't have tool dependency support so we must install them first.
 ~/.local/bin/mise use -g rust
