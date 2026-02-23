f06396fbff2209f50198377b2ed3f094dde75820
Peter Brunner <peter@lugoues.net>
2026-02-23 18:53:00 -0500

    add ghostty terminfo

--- chezmoiroot/.chezmoiscripts/run_before_02-ghostty-terminfo.sh
+++ chezmoiroot/.chezmoiscripts/run_before_02-ghostty-terminfo.sh
@@ -4,9 +4,11 @@ set -euo pipefail
 # ghostty's terminfo was accepted into ncurses but ncurses maintainer
 # didn't allow for `xterm-ghostty`.
 #
-terminfo_dir=/usr/share/terminfo
-G=${terminfo_dir}/g/ghostty
-XG=${terminfo_dir}/x/xterm-ghostty
+G=/usr/share/terminfo/g/ghostty
+XG="${HOME}/.terminfo/x/xterm-ghostty"
+
+
+mkdir -p "${HOME}/.terminfo/x"
 
 if [[ -r ${G} && ! -e ${XG} ]]; then
   ln -s ${G} ${XG}
