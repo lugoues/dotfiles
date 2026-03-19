e6e311ba0312385cf99fe593fd459e817989bb25
Peter Brunner <peter@lugoues.net>
2026-03-19 10:26:43 -0400

    don't use .local/bin/mise

--- chezmoiroot/.chezmoiscripts/run_after_00-mise.sh
+++ chezmoiroot/.chezmoiscripts/run_after_00-mise.sh
@@ -2,9 +2,9 @@
 
 mkdir -p ~/.local/{etc,bin,share/zsh/{init.d,completion}}
 
-~/.local/bin/mise self-update --yes
+mise self-update --yes
 
 # Mise doesn't have tool dependency support so we must install them first.
-~/.local/bin/mise use -g rust
+mise use -g rust
 
-~/.local/bin/mise install --yes
+mise install --yes
